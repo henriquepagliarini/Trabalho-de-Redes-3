f5b68de44baae21728557f8d3a14a086edf1a4fd
henriquepagliarini <120285612+henriquepagliarini@users.noreply.github.com>
2025-06-26 20:51:24 -0300

    Funcionamento da requisição HTTP pelo cliente no navegador web, com página de erro 404

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class ClientHandler implements Runnable {
     private Socket client;
@@ -15,19 +19,51 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         try (BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                PrintWriter output = new PrintWriter(client.getOutputStream(), true);) {
+                OutputStream output = client.getOutputStream()) {
 
             String line = input.readLine();
+            System.out.println("Requisição: " + line);
 
-            while ((line = input.readLine()) != null) {
-                System.out.println("Recebido: " + line);
-                if (line.isEmpty())
-                    break;
+            if (line == null || !line.startsWith("GET")) {
+                client.close();
+                return;
             }
 
-            output.println("Servidor recebeu a conexão!");
+            String[] chunks = line.split(" ");
+            String path = chunks[1].equals("/") ? "index.html" : chunks[1];
+            System.out.println(path);
+            String filePath = "files" + path;
+            System.out.println(filePath);
+
+            File file = new File(filePath);
+            if (file.exists()) {
+                sendHttpResponse(output, file, 200, "OK", "text/html");
+            } else {
+                File error404 = new File("files/notFound.html");
+                sendHttpResponse(output, error404, 404, "Not Found", "text/html");
+            }
+
+            client.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void sendHttpResponse(OutputStream output, File file, int code, String status, String contentType)
+            throws IOException {
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        PrintWriter writer = new PrintWriter(output, false);
+        writer.println("HTTP/1.1 " + code + " " + status);
+        writer.println("Server: ");
+        writer.println("Date: ");
+        writer.println("Content-Type: " + contentType);
+        writer.println("Last-Modified: ");
+        writer.println("Content-Length: " + bytes.length);
+        writer.println();
+        writer.flush();
+
+        output.write(bytes);
+        output.flush();
     }
 }
