765b0ff1d11228e2b6c333b2c352065cb35c3597
henriquepagliarini <120285612+henriquepagliarini@users.noreply.github.com>
2025-06-26 21:48:30 -0300

    Finalização do trabalho

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -6,14 +6,16 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ClientHandler implements Runnable {
     private Socket client;
-    private String clientAdress;
 
     public ClientHandler(Socket client) {
         this.client = client;
-        clientAdress = client.getInetAddress().getHostAddress();
     }
 
     @Override
@@ -22,48 +24,78 @@ public class ClientHandler implements Runnable {
                 OutputStream output = client.getOutputStream()) {
 
             String line = input.readLine();
-            System.out.println("Requisição: " + line);
+            System.out.println("Requisicao: " + line);
 
-            if (line == null || !line.startsWith("GET")) {
-                client.close();
+            if (line == null || line.isEmpty())
+                return;
+
+            String[] chunks = line.split(" ");
+            if (chunks.length < 2 || !chunks[0].equals("GET"))
+                return;
+
+            String path = chunks[1].equals("/") ? "index.html" : chunks[1];
+
+            if (path.contains("..") || path.contains("//")) {
+                File error404 = new File("files/notFound.html");
+                sendHttpResponse(output, error404, 404, "Not Found");
                 return;
             }
 
-            String[] chunks = line.split(" ");
-            String path = chunks[1].equals("/") ? "index.html" : chunks[1];
-            System.out.println(path);
             String filePath = "files" + path;
-            System.out.println(filePath);
 
             File file = new File(filePath);
             if (file.exists()) {
-                sendHttpResponse(output, file, 200, "OK", "text/html");
+                sendHttpResponse(output, file, 200, "OK");
             } else {
                 File error404 = new File("files/notFound.html");
-                sendHttpResponse(output, error404, 404, "Not Found", "text/html");
+                sendHttpResponse(output, error404, 404, "Not Found");
             }
-
-            client.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private void sendHttpResponse(OutputStream output, File file, int code, String status, String contentType)
-            throws IOException {
+    private void sendHttpResponse(OutputStream output, File file, int code, String status) throws IOException {
+        String mimeType = mimeTypeDetector(file.getName());
+
         byte[] bytes = Files.readAllBytes(file.toPath());
+
+        ZonedDateTime moment = ZonedDateTime.now(ZoneId.of("GMT"));
+        String dateHTTP = DateTimeFormatter.RFC_1123_DATE_TIME.format(moment);
+
+        FileTime lastFileModification = Files.getLastModifiedTime(file.toPath());
+        ZonedDateTime lastModificationTime = lastFileModification.toInstant().atZone(ZoneId.of("GMT"));
+        String lastModification = DateTimeFormatter.RFC_1123_DATE_TIME.format(lastModificationTime);
 
         PrintWriter writer = new PrintWriter(output, false);
         writer.println("HTTP/1.1 " + code + " " + status);
-        writer.println("Server: ");
-        writer.println("Date: ");
-        writer.println("Content-Type: " + contentType);
-        writer.println("Last-Modified: ");
+        writer.println("Server: ServidorJavaHenrique/1.0");
+        writer.println("Date: " + dateHTTP);
+        writer.println("Content-Type: " + mimeType);
+        writer.println("Last-Modified: " + lastModification);
         writer.println("Content-Length: " + bytes.length);
         writer.println();
         writer.flush();
 
         output.write(bytes);
         output.flush();
+    }
+
+    private String mimeTypeDetector(String fileName) {
+        if (fileName.endsWith(".html") || fileName.endsWith(".htm")) {
+            return "text/html";
+        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else if (fileName.endsWith(".png")) {
+            return "image/png";
+        } else {
+            return "application/octet-stream";
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -2,13 +2,9 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Server {
     private static final Integer port = 8080;
-
-    private static List<ClientHandler> clients = new CopyOnWriteArrayList<>();
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
@@ -18,9 +14,7 @@ public class Server {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Novo cliente conectado: (" + clientSocket.getInetAddress().getHostAddress() + ")");
 
-                ClientHandler client = new ClientHandler(clientSocket);
-                clients.add(client);
-                new Thread(client).start();
+                new Thread(new ClientHandler(clientSocket)).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
